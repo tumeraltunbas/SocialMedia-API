4be8d3523accf7fb04b0645443743145419323e3
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-01 13:19:36 +0300

    Update db.service

--- services/database/db.services.js
+++ services/database/db.services.js
@@ -3,14 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config({path: "./config/config.env"});
 
-const {DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DIALECT} = process.env;
+const {
+    DB_HOST, 
+    DB_PORT, 
+    DB_USER, 
+    DB_PASS, 
+    DB_DIALECT, 
+    DB_NAME
+} = process.env;
 
-const db = new Sequelize({
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
     host:DB_HOST,
     port: DB_PORT,
-    username: DB_USER,
-    password: DB_PASS,
-    dialect: DB_DIALECT
+    dialect: DB_DIALECT,
 });
 
 
