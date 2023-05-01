91e18384d67c7aa556bd31872b0f386c14a97496
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-01 14:01:14 +0300

    Create saveJwtToCookie helper

--- utils/tokenHelpers.js
+++ utils/tokenHelpers.js
@@ -11,3 +11,18 @@ export const createToken = () => {
     return hash;
     
 }
+
+export const saveJwtToCookie = (user, res) => {
+
+    const {COOKIE_EXPIRES, NODE_ENV} = process.env;
+
+    const jwt = user.createJwt();
+
+    return res
+    .status(200)
+    .cookie("jwt", jwt, {
+        maxAge: COOKIE_EXPIRES,
+        httpOnly: NODE_ENV === "development" ? true : false
+    });
+
+}
