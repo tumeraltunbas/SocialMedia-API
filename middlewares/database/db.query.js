e0e605ba3cd3a13fe4c87d3c6a045ce9eceb21c4
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-02 11:47:27 +0300

    Update checkUserExists

--- middlewares/database/db.query.js
+++ middlewares/database/db.query.js
@@ -1,20 +1,24 @@
 import expressAsyncHandler from "express-async-handler";
 import User from "../../models/User.js";
 import CustomError from "../../services/error/CustomError.js";
+import { Op } from "sequelize";
 
 export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
 
-    const {username} = req.body;
+    const key = req.body.username || req.body.email;
 
     const user = await User.findOne({
         where: {
-            username: username,
+            [Op.or]: [
+                {username: key},
+                {email: key}
+            ]
         },
         attributes: ["id"]
     });
 
     if(!user){
-        return next(new CustomError(404, "User not found with this username"));
+        return next(new CustomError(404, "User not found"));
     }
 
     next();
