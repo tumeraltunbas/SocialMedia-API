4fd8a81fcc5c7d281f83e668976c033cb5c5af16
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-03 14:45:03 +0300

    Add user and post relationship

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
 import User from "./User.js";
 import BackupCode from "./BackupCode.js";
 import db from "../services/database/db.services.js";
+import Post from "./Post.js";
 
 //User and BackupCode many to many
 User.belongsToMany(BackupCode, { through: "UserBackupCodes" });
 BackupCode.belongsToMany(User, { through: "UserBackupCodes" });
 
-
-
+//User and Post one to many
+Post.belongsTo(User, { foreignKey: "UserId" });
+User.hasMany(Post, { onDelete: "CASCADE" });
 
 await db.sync();
 export {User, BackupCode}
