fcc97b76891f0f5e9699437898f0b80691f47bd7
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-03 12:08:57 +0300

    Revert "Create upload profile image routeg" typo
    This reverts commit 749d17f78177605e28b932b58fc6a200fbf9ef44.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.js";
 import backupRoutes from "./backupCode.js";
-import userRoutes from "./user.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/backup", backupRoutes);
-router.use("/user", userRoutes);
 
 export default router;
