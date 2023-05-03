52c9af296bea467eda5c637093d4f2fbfc469fd9
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-03 15:13:00 +0300

    Create check post exists middleware

--- middlewares/database/db.query.js
+++ middlewares/database/db.query.js
@@ -2,6 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 import User from "../../models/User.js";
 import CustomError from "../../services/error/CustomError.js";
 import { Op } from "sequelize";
+import Post from "../../models/Post.js";
 
 export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
 
@@ -19,6 +20,26 @@ export const checkUserExists = expressAsyncHandler(async(req, res, next) => {
 
     if(!user){
         return next(new CustomError(404, "User not found"));
+    }
+
+    next();
+
+});
+
+export const checkPostExists = expressAsyncHandler(async(req, res, next) => {
+
+    const {postId} = req.params;
+
+    const post = await Post.findOne({
+        where: {
+            id: postId,
+            isVisible: true
+        },
+        attributes: ["id"]
+    });
+
+    if(!post){
+        return next(new CustomError(404, "Post not found"));
     }
 
     next();
