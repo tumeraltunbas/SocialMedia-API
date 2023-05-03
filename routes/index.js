749d17f78177605e28b932b58fc6a200fbf9ef44
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-03 12:08:34 +0300

    Create upload profile image routeg

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.js";
 import backupRoutes from "./backupCode.js";
+import userRoutes from "./user.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/backup", backupRoutes);
+router.use("/user", userRoutes);
 
 export default router;
