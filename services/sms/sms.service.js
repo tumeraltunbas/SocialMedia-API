37ac10e782f5273fee2368c64e0dd4ba70261286
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-03 12:58:34 +0300

    Create send phone code service

--- services/sms/sms.service.js
+++ services/sms/sms.service.js
@@ -1,5 +1,6 @@
 import twilio from "twilio";
 import expressAsyncHandler from "express-async-handler";
+import { generateOtp } from "../../utils/tokenHelpers.js";
 
 const {TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER} = process.env;
 
@@ -13,4 +14,18 @@ export const sendSms = expressAsyncHandler(async(phoneNumber, text) => {
         body: text
     });
    
+});
+
+export const sendPhoneCode = expressAsyncHandler(async(user) => {
+
+    const {PHONE_CODE_EXPIRES} = process.env;
+
+    const otp = generateOtp();
+    user.phoneCode = otp;
+    user.phoneCodeExpires = new Date(Date.now() + Number(PHONE_CODE_EXPIRES));
+
+    await user.save();
+
+    sendSms(user.phoneNumber, `Your phone code is ${user.phoneCode}. This code is valid for 30 minutes`);
+
 });
