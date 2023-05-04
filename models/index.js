8cdd36974cecdf73b0fae1c820193cdd3a214415
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-04 17:38:05 +0300

    Add post-like and user-like relationships

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import User from "./User.js";
 import BackupCode from "./BackupCode.js";
 import db from "../services/database/db.services.js";
 import Post from "./Post.js";
+import Like from "./Like.js";
 
 //User and BackupCode many to many
 User.belongsToMany(BackupCode, { through: "UserBackupCodes" });
@@ -10,6 +11,14 @@ BackupCode.belongsToMany(User, { through: "UserBackupCodes" });
 //User and Post one to many
 Post.belongsTo(User, { foreignKey: "UserId" });
 User.hasMany(Post, { onDelete: "CASCADE" });
+
+//User and like one to many
+User.hasMany(Like, { foreignKey: "UserId" });
+Like.belongsTo(User, { onDelete: "CASCADE" });
+
+//Post and like one to many
+Post.hasMany(Like, { foreignKey: "PostId" });
+Like.belongsTo(Post, { onDelete: "CASCADE" });
 
 await db.sync();
 export {User, BackupCode}
