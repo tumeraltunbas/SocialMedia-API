b1c7d61de1fc0e0a28822a8ad253fe961d3165d9
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-04 17:43:02 +0300

    Create undo like post route

--- controllers/like.js
+++ controllers/like.js
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import Like from "../models/Like.js";
+import CustomError from "../services/error/CustomError.js";
 
 export const likePost = expressAsyncHandler(async(req, res, next) => {
 
@@ -17,7 +18,33 @@ export const likePost = expressAsyncHandler(async(req, res, next) => {
     .json({
         success: true,
         message: "You successfully liked this post",
-        post: post
+    });
+
+});
+
+export const undoLikePost = expressAsyncHandler(async(req, res, next) => {
+
+    const {postId} = req.params;
+
+    const like = await Like.findOne({
+        where: {
+            PostId: postId,
+            UserId: req.user.id
+        },
+        attributes: ["id"]
+    });
+
+    if(!like){
+        return next(new CustomError(400, "You already did not like this post"));
+    }
+
+    await like.destroy();
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Like successfully withdrawn"
     });
 
 });
--- routes/like.js
+++ routes/like.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { isAuth } from "../middlewares/auth/auth.js";
 import { checkPostExists } from "../middlewares/database/db.query.js";
-import { likePost } from "../controllers/like.js";
+import { likePost, undoLikePost } from "../controllers/like.js";
 
 const router = Router({mergeParams: true});
 
 router.get("/", [isAuth, checkPostExists], likePost);
-
+router.get("/undo", [isAuth, checkPostExists], undoLikePost);
 
 export default router;
