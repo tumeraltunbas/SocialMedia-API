1ece1720c9f129c3b4894b68a1b75c4ff5e208a5
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-05 15:10:34 +0300

    Add get post owner access to route

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { isAuth } from "../middlewares/auth/auth.js";
+import { getPostOwnerAccess, isAuth } from "../middlewares/auth/auth.js";
 import { createPost, updatePost, hidePost, getPostById, getFeed } from "../controllers/post.js";
 import upload from "../services/file/upload.service.js";
 import { checkPostExists } from "../middlewares/database/db.query.js";
@@ -11,14 +11,13 @@ const router = Router();
 router.get("/feed", isAuth, getFeed);
 
 router.post("/", [isAuth, upload.single("file")], createPost);
-router.put("/:postId", [isAuth, checkPostExists], updatePost);
-router.put("/:postId/hide", [isAuth, checkPostExists], hidePost);
+router.put("/:postId", [isAuth, checkPostExists, getPostOwnerAccess], updatePost);
+router.put("/:postId/hide", [isAuth, checkPostExists, getPostOwnerAccess], hidePost);
 router.get("/:postId", [isAuth, checkPostExists], getPostById);
 
-
-//Like
+//Like routes
 router.use("/like/:postId", likeRoutes);
-//Comment
+//Comment routes
 router.use("/comment/:postId", commentRoutes);
 
 export default router;
