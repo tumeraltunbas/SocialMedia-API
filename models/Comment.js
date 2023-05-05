55da4d6c1c3b18c446bd02e9d7b3bf3ab4f35aa1
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-05 12:48:05 +0300

    Create comment model

---
+++ models/Comment.js
@@ -0,0 +1,30 @@
+import db from "../services/database/db.services.js";
+import { DataTypes } from "sequelize";
+
+const Comment = db.define("Comment", {
+
+    PostId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    UserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    isVisible: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    isHidByUser: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+
+}, {timestamps: false});
+
+await Comment.sync();
+export default Comment;
