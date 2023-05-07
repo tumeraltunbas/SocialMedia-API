c62568ac18aab029197836f02093510414cc6606
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-07 19:30:08 +0300

    Add cors

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import { errorHandler } from "./middlewares/error/errorHandler.js";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import "./models/index.js";
+import cors from "cors";
 
 dotenv.config({path: "./config/config.env"});
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({credentials: true}));
 app.use(express.static("public"));
 app.use("/api", routes);
 app.use(errorHandler);
