0818a5eb99dac783d856f898e792e6b8d7ae8c92
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-08 17:27:22 +0300

    Create get all posts route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import User from "../models/User.js";
+import Post from "../models/Post.js";
 
 export const getAllUsers = expressAsyncHandler(async(req, res, next) => {
 
@@ -28,5 +29,41 @@ export const getAllUsers = expressAsyncHandler(async(req, res, next) => {
         users: users,
         pagination: pagination
     });
+
+});
+
+export const getAllPosts = expressAsyncHandler(async(req, res, next) => {
+
+    const {startIndex, limit, pagination, where} = req.postQuery;
+
+    const posts = await Post.findAll({
+        where: where,
+        offset: startIndex,
+        limit: limit,
+        order: [["createdAt", "asc"]],
+        attributes: [
+            "id",
+            "content",
+            "imageUrl",
+            "UserId",
+            "isVisible"
+        ],
+        include: {
+            model: User,
+            attributes: [
+                "id",
+                "username"
+            ]
+        }
+    });
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        posts: posts,
+        pagination: pagination
+    });
+
 
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getAdminAccess, isAuth } from "../middlewares/auth/auth.js";
 import { userQueryMiddleware } from "../middlewares/database/userQueryMiddleware.js";
-import { getAllUsers } from "../controllers/admin.js";
+import { getAllUsers, getAllPosts } from "../controllers/admin.js";
+import { postQueryMiddleware } from "../middlewares/database/postQueryMiddleware.js";
 
 const router = Router();
 
 router.get("/users", [isAuth, getAdminAccess, userQueryMiddleware], getAllUsers);
+router.get("/posts", [isAuth, getAdminAccess, postQueryMiddleware], getAllPosts);
 
 
 export default router;
