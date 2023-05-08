223e709173082625c9a2615c1a7d7232b25a138d
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-08 16:44:00 +0300

    Create user query middleware

---
+++ middlewares/database/userQueryMiddleware.js
@@ -0,0 +1,20 @@
+import expressAsyncHandler from "express-async-handler";
+import User from "../../models/User.js";
+import { paginationHelper, userSearchHelper } from "./queryMiddlewareHelpers.js";
+
+export const userQueryMiddleware = expressAsyncHandler(async(req, res, next) => {
+
+    const {startIndex, limit, pagination} = await paginationHelper(req, User);
+
+    const {where} = userSearchHelper(req);
+
+    req.userQuery = {
+        startIndex,
+        limit,
+        pagination,
+        where
+    };
+
+    next();
+
+});
