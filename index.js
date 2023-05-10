e2c09334d901fe9136c4af3b03647acd4677389f
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-10 19:46:14 +0300

    Create ws server

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import "./models/index.js";
 import cors from "cors";
+import { WebSocketServer } from "ws";
+import {createServer} from "http2";
 
 dotenv.config({path: "./config/config.env"});
 const app = express();
 
+const server = createServer(app);
+const wss = new WebSocketServer({server: server});
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({credentials: true, origin: true}));
 app.use(express.static("public"));
 app.use("/api", routes);
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
+wss.on("connection", (socket) => {
+    console.log(`${socket} connected`);
+});
+
+server.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
