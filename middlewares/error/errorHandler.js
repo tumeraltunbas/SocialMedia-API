674cd4ad06bc774d35ab5e3a0167839e8e50a3ad
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-17 20:06:43 +0300

    Update error handler middleware

--- middlewares/error/errorHandler.js
+++ middlewares/error/errorHandler.js
@@ -1,7 +1,12 @@
 import CustomError from "../../services/error/CustomError.js";
+import {capitalize} from "../../utils/inputHelpers.js"
 
 export const errorHandler = (err, req, res, next) => {
     
+    if(err.name === "SequelizeUniqueConstraintError"){
+        err.message = capitalize(err.errors[0].message);
+    }
+
     return res
     .status(err.status || 500)
     .json({
