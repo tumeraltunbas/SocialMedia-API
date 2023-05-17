76336a940304e6447729ddf4af1b3c8d893c7a31
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-17 19:49:52 +0300

    Update routes due to update on upload service

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getPostOwnerAccess, isAuth } from "../middlewares/auth/auth.js";
 import { createPost, updatePost, hidePost, getPostById, getFeed } from "../controllers/post.js";
-import upload from "../services/file/upload.service.js";
+import { uploader } from "../services/file/upload.service.js";
 import { checkPostExists, checkPostBelongsToBlockedUser } from "../middlewares/database/db.query.js";
 import likeRoutes from "./like.js";
 import commentRoutes from "./comment.js";
@@ -11,7 +11,7 @@ const router = Router();
 
 router.get("/feed", [isAuth, postQueryMiddleware], getFeed);
 
-router.post("/", [isAuth, upload.single("file")], createPost);
+router.post("/", [isAuth, uploader.single("file")], createPost);
 router.put("/:postId", [isAuth, checkPostExists, getPostOwnerAccess], updatePost);
 router.put("/:postId/hide", [isAuth, checkPostExists, getPostOwnerAccess], hidePost);
 router.get("/:postId", [isAuth, checkPostExists, checkPostBelongsToBlockedUser], getPostById);
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { isAuth } from "../middlewares/auth/auth.js";
-import upload from "../services/file/upload.service.js";
+import {imageUploader} from "../services/file/upload.service.js";
 import { uploadProfileImage, removeProfileImage, updateProfile, addPhoneNumber, changePhoneNumber, changeEmail, getLikedPosts, followUser, unfollowUser, getProfile, getFollowings, getFollowers, blockUser, unblockUser, getBlocks, unblockAll } from "../controllers/user.js";
 import { checkUserExists, checkUserFollowing, checkUserBlocked } from "../middlewares/database/db.query.js";
 
 const router = Router();
 
-router.post("/profile/image", [isAuth, upload.single("file")], uploadProfileImage);
+router.post("/profile/image", [isAuth, imageUploader.single("file")], uploadProfileImage);
 router.put("/profile/image/remove", isAuth, removeProfileImage);
 router.put("/profile/update", isAuth, updateProfile);
 router.post("/phone/add", isAuth, addPhoneNumber);
