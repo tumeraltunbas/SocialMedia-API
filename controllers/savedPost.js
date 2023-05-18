a6027e56d92442450972b3fcc9f7cdf56f8d6492
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-18 15:47:31 +0300

    Create undo save post route

--- controllers/savedPost.js
+++ controllers/savedPost.js
@@ -24,10 +24,36 @@ export const savePost = expressAsyncHandler(async(req, res, next) => {
     });
 
     return res
-    .status(200)
+    .status(201)
     .json({
         success: true,
         message: "The post has been saved"
+    });
+
+});
+
+export const undoSavePost = expressAsyncHandler(async(req, res, next) => {
+
+    const { postId } = req.params;
+
+    const savedPost = await SavedPost.findOne({
+        where: {
+            PostId: postId,
+            UserId: req.user.id 
+        }
+    });
+
+    if(!savedPost){
+        return next(new CustomError(400, "You already did not save this post"));
+    }
+
+    await savedPost.destroy();
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Post has been successfully unsaved"
     });
 
 });
--- routes/savedPost.js
+++ routes/savedPost.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { isAuth } from "../middlewares/auth/auth.js";
 import { checkPostBelongsToBlockedUser, checkPostExists } from "../middlewares/database/db.query.js";
-import { savePost } from "../controllers/savedPost.js";
+import { savePost, undoSavePost } from "../controllers/savedPost.js";
 
 const router = Router({mergeParams: true});
 
 router.get("/", [isAuth, checkPostExists, checkPostBelongsToBlockedUser], savePost);
+router.delete("/", [isAuth, checkPostExists], undoSavePost);
 
 export default router;
