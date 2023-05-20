446c19ed86afeab03258b31c1450373ecd0c53fc
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-20 14:33:52 +0300

    Update update comment route

--- routes/comment.js
+++ routes/comment.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { isAuth, getCommentOwnerAccess } from "../middlewares/auth/auth.js";
 import { getCommentsByPostId, createComment, updateComment, hideComment } from "../controllers/comment.js";
-import { checkPostExists, checkCommentExists, checkPostBelongsToBlockedUser, checkPostBelongsToUser, checkProfileAccess} from "../middlewares/database/db.query.js";
+import { checkPostExists, checkCommentExists, checkPostBelongsToUser, checkProfileAccess} from "../middlewares/database/db.query.js";
 import { uploader } from "../services/file/upload.service.js";
 
 const router = Router({mergeParams: true});
@@ -9,7 +9,7 @@ const router = Router({mergeParams: true});
 router.use(isAuth);
 
 router.post("/:username/:postId", [checkPostBelongsToUser, checkProfileAccess, uploader.single("file")], createComment);
-router.put("/:username/:postId/:commentId", [checkPostBelongsToBlockedUser, checkProfileAccess, checkCommentExists, getCommentOwnerAccess], updateComment);
+router.put("/:username/:postId/:commentId", [checkProfileAccess, checkCommentExists, getCommentOwnerAccess], updateComment);
 router.put("/:postId/:commentId/hide", [checkCommentExists, getCommentOwnerAccess], hideComment);
 router.get("/:username/:postId", [checkPostExists, checkProfileAccess], getCommentsByPostId);
 
