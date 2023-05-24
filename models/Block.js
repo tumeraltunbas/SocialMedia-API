42553b93391d8ab4ef9ba91743fc5172d46cdb44
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-24 13:51:50 +0300

    Add after create and after destroy hooks

--- models/Block.js
+++ models/Block.js
@@ -1,5 +1,8 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Op } from "sequelize";
 import db from "../services/database/db.services.js";
+import Like from "./Like.js";
+import Post from "./Post.js";
+import Comment from "./Comment.js";
 
 const Block = db.define("Block", {
 
@@ -17,6 +20,138 @@ const Block = db.define("Block", {
     }
 
 }, {timestamps: false});
+
+
+Block.addHook("afterCreate", async function(block){
+
+    const likes = await Like.findAll({
+        where: {
+            [Op.or]: [
+                { UserId: block.BlockerId },
+                { UserId: block.BlockedId }
+            ]
+        },
+        include: {
+            model: Post,
+            where: {
+                [Op.or] : [
+                    { UserId: block.BlockedId },
+                    { UserId: block.BlockerId}
+                ]
+            },
+            attributes: [] 
+        },
+        attributes: [
+            "id",
+            "isVisible"
+        ]
+    });
+
+
+    const comments = await Comment.findAll({
+        where: {
+            [Op.or]: [
+                { UserId: block.BlockerId },
+                { UserId: block.BlockedId }
+            ]
+        },
+        include: {
+            model: Post,
+            where: {
+                [Op.or] : [
+                    { UserId: block.BlockedId },
+                    { UserId: block.BlockerId}
+                ]
+            },
+            attributes: [] 
+        },
+        attributes: [
+            "id",
+            "content",
+        ]
+    });
+
+    likes.forEach(async(like) => {
+
+        like.update({isVisible: false});
+        await like.save();
+
+    });
+
+    comments.forEach(async(comment) => {
+
+        comment.update({isVisible: false});
+        await comment.save();
+        
+    });
+
+});
+
+
+Block.addHook("afterDestroy", async function(block){
+
+    const likes = await Like.findAll({
+        where: {
+            [Op.or]: [
+                { UserId: block.BlockerId },
+                { UserId: block.BlockedId }
+            ]
+        },
+        include: {
+            model: Post,
+            where: {
+                [Op.or] : [
+                    { UserId: block.BlockedId },
+                    { UserId: block.BlockerId}
+                ]
+            },
+            attributes: [] 
+        },
+        attributes: [
+            "id",
+            "isVisible"
+        ]
+    });
+
+
+    const comments = await Comment.findAll({
+        where: {
+            [Op.or]: [
+                { UserId: block.BlockerId },
+                { UserId: block.BlockedId }
+            ]
+        },
+        include: {
+            model: Post,
+            where: {
+                [Op.or] : [
+                    { UserId: block.BlockedId },
+                    { UserId: block.BlockerId}
+                ]
+            },
+            attributes: [] 
+        },
+        attributes: [
+            "id",
+            "content",
+        ]
+    });
+
+    likes.forEach(async(like) => {
+
+        like.update({isVisible: true});
+        await like.save();
+
+    });
+
+    comments.forEach(async(comment) => {
+
+        comment.update({isVisible: true});
+        await comment.save();
+        
+    });
+
+});
 
 await Block.sync();
 export default Block;
