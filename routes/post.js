fa6037960aaa567fd969ebc9a8ecf61ae2a23cd1
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-24 14:11:14 +0300

    Bug fix v2

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getPostOwnerAccess, isAuth } from "../middlewares/auth/auth.js";
 import { getFeed, createPost, updatePost, hidePost, getPostById, getPostAsQr } from "../controllers/post.js";
 import { uploader } from "../services/file/upload.service.js";
-import { checkPostExists, checkProfileAccess } from "../middlewares/database/db.query.js";
+import { checkPostExists, checkProfileAccess, checkPostBelongsToBlockedUser } from "../middlewares/database/db.query.js";
 import { postQueryMiddleware } from "../middlewares/database/postQueryMiddleware.js";
 
 const router = Router();
@@ -12,7 +12,7 @@ router.get("/feed", [isAuth, postQueryMiddleware], getFeed);
 router.post("/", [isAuth, uploader.single("file")], createPost);
 router.put("/:postId", [isAuth, checkPostExists, getPostOwnerAccess], updatePost);
 router.put("/:postId/hide", [isAuth, checkPostExists, getPostOwnerAccess], hidePost);
-router.get("/:username/:postId", [isAuth, checkPostExists, checkProfileAccess], getPostById);
+router.get("/:username/:postId", [isAuth, checkPostExists, checkPostBelongsToBlockedUser, checkProfileAccess], getPostById);
 router.get("/:username/:postId/qr", [isAuth, checkPostExists, checkProfileAccess], getPostAsQr)
 
 export default router;
