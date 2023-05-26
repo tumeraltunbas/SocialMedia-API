81820b07464011d035737905148cbc20d35667ff
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-26 15:30:18 +0300

    Create Message model

---
+++ models/Message.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import db from "../services/database/db.services.js";
+
+const Message = db.define("Message", {
+
+    senderId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    recipientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    roomId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    isVisible: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+
+}, { timestamps: false });
+
+await Message.sync();
+export default Message;
