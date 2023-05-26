1caa18d3e72c7194cc02838067519611fb71a5f6
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-26 15:22:28 +0300

    Update message service

--- services/message/message.services.js
+++ services/message/message.services.js
@@ -1,42 +1,40 @@
 import Room from "../../models/Room.js";
 import Message from "../../models/Message.js";
 import User from "../../models/User.js";
+import db from "../database/db.services.js";
 
 export const sendMessage = async(req, message) => {
 
-    const participants = req.url.split("/")[1];
-    const userIds = participants.split("-");
-   
-    const userOneId = userIds[0];
-    const userTwoId = userIds[1];
+    const senderId = req.user.id;
+    const recipientId = req.url.split("/")[1];
 
     let room;
-
     
     //Users' room
     room = await Room.findOne({
         include: {
             model: User,
             where: {
-                id: [userOneId, userTwoId]
+                id: [senderId, recipientId]
             },
             attributes: ["id"]
-        }
+        },
+        group: ['Room.id'],
+        having: db.literal(`COUNT(*) = 2`)
     });
 
     if(!room){
 
-        //create new room
-
-        await Room.create({});
-        room.addUser(userOneId, userTwoId);
+        // create new room
+        room = await Room.create({});
+        room.addUser([senderId, recipientId]);
 
     }
 
     //Create message
     await Message.create({
-        senderId: userOneId,
-        recipientId: userTwoId,
+        senderId: senderId,
+        recipientId: recipientId,
         roomId: room.id,
         text: message
     });
