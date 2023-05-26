49f6750c3b75e1bdd3d21115a1d13ad0cd4ea66c
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-26 14:50:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import "./models/index.js";
 import cors from "cors";
-import { WebSocketServer, WebSocket } from "ws";
-import {createServer} from "http2";
+import { WebSocketServer } from "ws";
 import { sendMessage } from "./services/message/message.services.js";
-import { isAuth } from "./middlewares/auth/auth.js";
 
 dotenv.config({path: "./config/config.env"});
 const app = express();
 
-const server = createServer(app);
 const wss = new WebSocketServer({port: 8081});
 
 app.use(express.json());
@@ -37,4 +34,4 @@ wss.on("connection", (ws, req) => {
 
 });
 
-server.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
