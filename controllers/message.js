acf9c7f4188379ecdc27e2107261ca2393b88d98
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-27 16:03:19 +0300

    Create delete message route

--- controllers/message.js
+++ controllers/message.js
@@ -45,3 +45,25 @@ export const getMessagesByRoomId = expressAsyncHandler(async(req, res, next) => 
     });
 
 });
+
+export const deleteMessage = expressAsyncHandler(async(req, res, next) => {
+
+    const {roomId, messageId} = req.params;
+    
+    const message = await Message.findOne({
+        where: {
+            id: messageId,
+            RoomId: roomId
+        }
+    });
+
+    await message.destroy();
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        message: "Message has been deleted"
+    });
+
+});
--- routes/message.js
+++ routes/message.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { isAuth } from "../middlewares/auth/auth.js";
-import { checkRoomExists, checkIsRoomMember } from "../middlewares/database/db.query.js";
-import { getMessagesByRoomId } from "../controllers/message.js";
+import { isAuth, getMessageOwnerAccess } from "../middlewares/auth/auth.js";
+import { checkRoomExists, checkIsRoomMember, checkMessageExists } from "../middlewares/database/db.query.js";
+import { getMessagesByRoomId, deleteMessage } from "../controllers/message.js";
 
 
 const router = Router();
 
 router.get("/:roomId", [isAuth, checkRoomExists, checkIsRoomMember], getMessagesByRoomId);
+router.delete("/:roomId/:messageId", [isAuth, checkRoomExists, checkIsRoomMember, checkMessageExists, getMessageOwnerAccess], deleteMessage);
 
 export default router;
