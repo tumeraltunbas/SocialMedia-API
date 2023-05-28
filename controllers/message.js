259f5b60ff5580c549201ea1006583521a5b0189
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-05-28 12:24:00 +0300

    Get rooms by user route

--- controllers/message.js
+++ controllers/message.js
@@ -1,6 +1,50 @@
 import expressAsyncHandler from "express-async-handler";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import Room from "../models/Room.js";
+
+export const getRoomsByUser = expressAsyncHandler(async(req, res, next) => {
+
+    //Rooms that user subscribed
+    const user = await User.findOne({
+        where: {
+            id: req.user.id
+        },
+        attributes: ["id"],
+        include: {
+            model: Room,
+            attributes: ["id"]
+        }
+    });
+
+    //Room ids that user subscibed
+    const subscribedRoomIds = user.Rooms.map(r => r.id);
+
+    const users = await User.findAll({
+        include: {
+            model: Room,
+            where: {
+                id: subscribedRoomIds
+            },
+            attributes: ["id"]
+        },
+        attributes: [
+            "id",
+            "username",
+            "profileImageUrl"
+        ]
+    });
+
+    const filteredUsers = users.filter(u => u.id !== req.user.id);
+
+    return res
+    .status(200)
+    .json({
+        success: true,
+        users: filteredUsers,
+    });
+
+});
 
 export const getMessagesByRoomId = expressAsyncHandler(async(req, res, next) => {
 
